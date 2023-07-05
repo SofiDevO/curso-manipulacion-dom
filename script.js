b92061501c09e05c321e7a6025c318af1903e5bf
SofiDevO <angiemeow89@gmail.com>
2023-07-04 20:23:52 -0600

    clase 03 completada

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ const d = document;
 const btn = d.querySelector('[data-form-btn]');
 const createTask = (evento)=>{
     evento.preventDefault();    const input = d.querySelector('[data-form-input]');
-        console.log(input.value);
-    }
+    const value = input.value;
+    const list = d.querySelector("[data-list]")
+    const task = d.createElement("li")
+    task.classList.add("card")
+    input.value = ''; 
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;  
+
+  list.appendChild(task)
+    console.log(content);
+  }
+
+  btn.addEventListener('click', createTask)
 
 
-
-btn.addEventListener('click', createTask)
