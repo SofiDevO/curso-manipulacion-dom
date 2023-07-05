582f81f9127c38b561c7e7db8a785ab74f72791e
SofiDevO <angiemeow89@gmail.com>
2023-07-05 13:55:43 -0600

    clase 04 completada

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 const d = document;
 
-const btn = d.querySelector('[data-form-btn]');
-const createTask = (evento)=>{
-    evento.preventDefault();    const input = d.querySelector('[data-form-input]');
-    const value = input.value;
-    const list = d.querySelector("[data-list]")
-    const task = d.createElement("li")
-    task.classList.add("card")
-    input.value = ''; 
-    const content = ` <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  task.innerHTML = content;  
+(()=>{
+  const btn = d.querySelector('[data-form-btn]');
+  const createTask = (evento)=>{
+      evento.preventDefault();    const input = d.querySelector('[data-form-input]');
+      const value = input.value;
+      const list = d.querySelector("[data-list]")
+      const task = d.createElement("li")
+      task.classList.add("card")
+      input.value = ''; 
+      const taskContent = d.createElement("div");
+      taskContent.appendChild(checkComplete( ));
+      const titleTask = d.createElement('span');
+      titleTask.classList.add("task");
+      titleTask.innerText = value; 
+      taskContent.appendChild(titleTask);
 
-  list.appendChild(task)
-    console.log(content);
-  }
+      const content = ` 
+      <span class="task">${value}</span>
+      <i class="fas fa-trash-alt trashIcon icon"></i>`;
+      /* task.innerHTML = content;   */
+      task.appendChild(taskContent) 
+      list.appendChild(task)
+      }
 
-  btn.addEventListener('click', createTask)
 
+    btn.addEventListener('click', createTask);
 
+  const checkComplete = ()=>{
+    const i = d.createElement('i');
+    i.classList.add("far")
+    i.classList.add("fa-check-square")
+    i.classList.add("icon")
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const completeTask = (event)=>{
+    const element = event.target;
+    element.classList.toggle("fa");
+    element.classList.toggle("completeIcon")
+    element.classList.toggle("far")
+  } 
+})();
